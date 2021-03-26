a908711dd8b3879cd9ebe473792882ad27144cae
Stefan Wintergerst <stefan.wintergerst@uni-ulm.de>
2021-03-26 16:25:56 +0100

    add filter-images-ar script

---
+++ cmd/filter-images-ar/README.md
@@ -0,0 +1,13 @@
+# filter-aspect-ratio 📷
+Copy all images in the current directory to a new folder based on their aspect ratio,  
+i.e. widescreen (`width >= height`) and portrait (`width < height`).
+
+
+## Usage
+`$ > filter-aspect-ratio -quiet`  
+`$ > filter-aspect-ratio -portraitDir="phone" -wideScreenDir="desktop"`
+
+## Arguments
+- `-quiet` disables logging output
+- `-portraitDir (default : "./portrait")` sets the target directory for portrait images.
+- `-wideScreenDir (default : "./widescreen")` sets the target directory for widescreen images.
---
+++ cmd/filter-images-ar/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"path/filepath"
+	"time"
+
+	"github.com/StefanWin/file-util/v2/pkg"
+)
+
+func main() {
+
+	start := time.Now()
+
+	var quiet bool
+	flag.BoolVar(&quiet, "quiet", false, "Disable output")
+
+	var portraitDir string
+	flag.StringVar(&portraitDir, "portraitDir", "./portrait", "Directory for portrait images.")
+
+	var wideScreenDir string
+	flag.StringVar(&wideScreenDir, "wideScreenDir", "./widescreen", "Directory for widescreen images.")
+
+	flag.Parse()
+
+	logInfo := func(format string, v ...interface{}) {
+		if !quiet {
+			log.Printf(format, v...)
+		}
+	}
+
+	files, err := pkg.ListImagesInDirectory(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	count := len(files)
+	logInfo("found %d image files\n", count)
+
+	if err := pkg.EnsureDir(portraitDir); err != nil {
+		log.Fatal(err)
+	}
+
+	logInfo("created directorty %s\n", portraitDir)
+
+	if err := pkg.EnsureDir(wideScreenDir); err != nil {
+		log.Fatal(err)
+	}
+
+	logInfo("created directorty %s\n", wideScreenDir)
+
+	wideScreenCount := 0
+	portraitCount := 0
+	aspectRationSum := 0.0
+
+	for _, f := range files {
+		src := filepath.Join("./", f.Name())
+		dst := ""
+		w, h, err := pkg.GetImageBounds(src)
+		if err != nil {
+			log.Fatal(err)
+		}
+		aspectRationSum += float64(w / h)
+		if w >= h {
+			dst = filepath.Join("./", wideScreenDir, f.Name())
+			wideScreenCount += 1
+		} else {
+			dst = filepath.Join("./", portraitDir, f.Name())
+			portraitCount += 1
+		}
+		if err := pkg.CopyFile(src, dst); err != nil {
+			log.Fatal(err)
+		}
+		logInfo("copied :: (%dx%d) %s => %s\n", w, h, src, dst)
+	}
+
+	avgAspectRatio := aspectRationSum / float64(count)
+	logInfo("average aspect ratio : %f\n", avgAspectRatio)
+
+	elapsed := time.Since(start)
+	logInfo("successfully filtered %d (%d widescreen, %d portrait) in %s\n", count, wideScreenCount, portraitCount, elapsed)
+
+}
